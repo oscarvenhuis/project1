5a438fa2f0b2ce6fc89b950b4572da584a1dc3a8
Oscar Venhuis <Oscar@Oscars-MacBook-Pro.local>
2016-06-08 14:22:45 +0800

    version Wed 9 Jun

--- scripts/game.js
+++ scripts/game.js
@@ -21,7 +21,34 @@
 // set variable,
 // change the score multiplier
 
+// gravity
+// set x position
+// set y position
+// set starting gravity speed
+// set increase of speed for each frame
+// calculate trajectory
+// loop until it hits the paddle
+//
+// function gravity(width, height, color, x, y, type) {
+//     this.type = type;
+//     this.width = width;
+//     this.height = height;
+//     this.x = x;
+//     this.y = y;
+//     this.speedX = 0;
+//     this.speedY = 0;
+//     this.gravity = 0.05;
+//     this.gravitySpeed = 0;
+//     this.update = function() {
 
+//     }
+// new position of ball
+//     this.newPos = function() {
+//         this.gravitySpeed += this.gravity;
+//         this.x += this.speedX;
+//         this.y += this.speedY + this.gravitySpeed;
+//     }
+// }
 
 
 $(document).ready(function(){
@@ -29,13 +56,19 @@ $(document).ready(function(){
   var gameloop = null;
 
   // Players 1 & 2
-  var $player1 = $("#paddleA");
-  var $player2 = $("#paddleB");
+  var $paddleA     = $("#paddleA");
+  var $paddleB     = $("#paddleB");
+  var paddleSpeed  = 5;
+  var paddleHeight = 15;
+  var paddleWidth  = 100;
 
   // Ball
   var $ball          = $("#ball");
-  var verticalMove   = 3;
-  var horizontalMove = 3;
+  var verticalMove   = 5;
+  var horizontalMove = 5;
+  var ballHeight     = 100;
+  var ballWidth      = 57;
+  var lastContact    = '';
 
   //Controls
   var keyBindings = { p1Left: 90, p1Right: 88, p2Left: 37, p2Right: 39};
@@ -44,14 +77,11 @@ $(document).ready(function(){
 
   // Default Settings
   var gameHeight   = 600;
-  var gameWidth    = 800;
+  var gameWidth    = 1000;
   var xMin         = 0;
-  var xMax         = 800;
+  var xMax         = 1000;
   var yMin         = 0;
   var yMax         = 600;
-  var paddleSpeed  = 5;
-  var paddleHeight = 15;
-  var paddleWidth  = 100;
 
   // Score needed to win game
   var winScore = 5;
@@ -99,96 +129,124 @@ $(document).ready(function(){
     });
   };
 
+
   // Control movement of paddles based on keyboard events
   var movePaddles = function () {
     // Check keyboard events
-    var p1Position = $player1.position();
+    var p1Position = $paddleA.position();
     var p1Left     = p1Position.left;
-    var p1Right    = p1Position.left + 100;
+    var p1Right    = p1Position.left + paddleWidth;
 
-    var p2Position = $player2.position();
+    var p2Position = $paddleB.position();
     var p2Left     = p2Position.left;
-    var p2Right    = p2Position.left + 100;
+    var p2Right    = p2Position.left + paddleWidth;
 
     // Move the paddle A left
     if (p1Movement.left && p1Left > xMin && p1Left - paddleSpeed >= xMin) {
-      $player1.css("left", p1Position.left - paddleSpeed);
+      $paddleA.css("left", p1Position.left - paddleSpeed);
     } else if (p1Left - paddleSpeed < xMin) {
-      $player1.css("left", xMin);
+      $paddleA.css("left", xMin);
     }
 
     // Move the paddle A right
     if (p1Movement.right && p1Right < xMax && p1Right + paddleSpeed <= xMax) {
-      $player1.css("left", p1Position.left + paddleSpeed);
+      $paddleA.css("left", p1Position.left + paddleSpeed);
     } else if (p1Right - paddleSpeed > xMax) {
-      $player1.css("left", xMax - paddleWidth);
+      $paddleA.css("left", xMax - paddleWidth);
     }
 
     // Move the paddle B left
     if (p2Movement.left && p2Left > xMin && p2Left - paddleSpeed >= xMin) {
-      $player2.css("left", p2Position.left - paddleSpeed);
+      $paddleB.css("left", p2Position.left - paddleSpeed);
     } else if (p2Left - paddleSpeed < xMin) {
-      $player1.css("left", xMin);
+      $paddleB.css("left", xMin);
     }
 
     // Move the paddle B right
     if (p2Movement.right && p2Right < xMax && p2Right + paddleSpeed <= xMax) {
-      $player2.css("left", p2Position.left + paddleSpeed);
+      $paddleB.css("left", p2Position.left + paddleSpeed);
     } else if (p2Right - paddleSpeed > xMax) {
-      $player2.css("left", xMax - paddleWidth);
+      $paddleB.css("left", xMax - paddleWidth);
     }
-
   };
 
-// gravity
-// set x position
-// set y position
-// set starting gravity speed
-// set increase of speed for each frame
-// calculate trajectory
-// loop until it hits the paddle
-//
-// function gravity(width, height, color, x, y, type) {
-//     this.type = type;
-//     this.width = width;
-//     this.height = height;
-//     this.x = x;
-//     this.y = y;
-//     this.speedX = 0;
-//     this.speedY = 0;
-//     this.gravity = 0.05;
-//     this.gravitySpeed = 0;
-//     this.update = function() {
+  // ball positioning
+  var moveBall = function (){
+    // ball boundery collision
+    var position = $ball.position();
+    var ballTop  = position.top;
+    var ballBot  = position.top + ballHeight;
+    var ballLeft = position.left;
+    var ballRight= position.left + ballWidth;
 
-//     }
-// new position of ball
-//     this.newPos = function() {
-//         this.gravitySpeed += this.gravity;
-//         this.x += this.speedX;
-//         this.y += this.speedY + this.gravitySpeed;
-//     }
-// }
+    // boundary collision
+    if (ballTop <= yMin && lastContact != "topBoundary") {
+      verticalMove = -verticalMove;
+      lastContact = "topBoundary";
+    }
 
-   var moveBall = function () {
-     var position = $ball.position();
-     $ball.css({
-       top:  position.top + verticalMove,
-       left: position.left + horizontalMove
-     });
-   };
+    if (ballBot >= yMax && lastContact != "botBoundary") {
+       verticalMove = -verticalMove;
+      lastContact = "botBoundary";
+      // clearInterval(gameloop);
+      // check winner and add score
+      // reset the horizontal and vertical move to default
+      // reset lastContact to default
+      // reset the cat position
+      // start gameloop
+    }
+
+    if (ballLeft <= xMin && lastContact != "leftBoundary") {
+      horizontalMove = -horizontalMove;
+      lastContact = "leftBoundary";
+    }
+
+    if (ballRight >= xMax && lastContact != "rightBoundary") {
+      horizontalMove = -horizontalMove;
+      lastContact = "rightBoundary";
+    }
+
+    // paddleA collision
+    var paddleAPosition = $paddleA.position();
+    var paddleATop      = paddleAPosition.top;
+    var paddleABot      = paddleAPosition.top + paddleHeight;
+    var paddleALeft     = paddleAPosition.left;
+    var paddleARight    = paddleAPosition.left + paddleWidth;
+
+    if (ballLeft <= paddleARight && ballRight >= paddleALeft && ballTop <= paddleABot && ballBot >= paddleATop && lastContact !== 'paddleA') {
+      verticalMove = -verticalMove;
+      lastContact = 'paddleA';
+    }
+
+    // paddleB collision
+    var paddleBPosition = $paddleB.position();
+    var paddleBTop      = paddleBPosition.top;
+    var paddleBBot      = paddleBPosition.top + paddleHeight;
+    var paddleBLeft     = paddleBPosition.left;
+    var paddleBRight    = paddleBPosition.left + paddleWidth;
+
+    if (ballLeft <= paddleBRight && ballRight >= paddleBLeft && ballTop <= paddleBBot && ballBot >= paddleBTop && lastContact !== 'paddleB') {
+      verticalMove = -verticalMove;
+      lastContact = 'paddleB';
+    }
+
+    $ball.css({
+       top:  ballTop + verticalMove,
+       left: ballLeft + horizontalMove
+    });
+  };
+
 
   var startGame = function () {
     // Main loop of the game
     // Set main loop frame rate (60 fps the best). Fps stands for 'frames per second'
     gameloop = setInterval(function(){
-      movePaddles();
       moveBall();
+      movePaddles();
     }, 1000 / 60);
 
     bindKeypress();
   };
 
   startGame();
-
-
 });
